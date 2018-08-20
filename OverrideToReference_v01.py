e727bd1c9af5e88febc3c4b9b11f815676b7cf9b
sissy <sissywhitfield@gmail.com>
2018-08-20 13:11:44 -0400

    Override Display to Reference

--- OverrideToReference_v01.py
+++ OverrideToReference_v01.py
@@ -1,8 +1,7 @@
 import maya.cmds as mc
 
-selection = mc.ls(sl=True);
-
 def OverrideToReference():
+    selection = mc.ls(sl=True);
     for x in selection:
         mc.setAttr(x + '.overrideEnabled', 1);
         mc.setAttr(x + '.overrideDisplayType', 2);
